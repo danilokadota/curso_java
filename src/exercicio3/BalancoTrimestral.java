2bfc5b35edb171f6c17dbd8220bd5042387f7e6b
danilokadota <danilo.kadota@dclick.com.br>
2013-09-03 16:59:25 -0300

    curso_java

---
+++ src/exercicio3/BalancoTrimestral.java
@@ -0,0 +1,16 @@
+package exercicio3;
+
+public class BalancoTrimestral {
+
+	public static void main(String[] args) {
+		int gastosJaneiro = 1500;
+		int gastosFevereiro = 2000;
+		int gastosMarco = 1700;
+		int gastroTrimestral = gastosJaneiro + gastosFevereiro + gastosMarco;
+		
+	System.out.println("Valor da media mensal = " + gastroTrimestral);
+	
+	
+	}
+
+}
